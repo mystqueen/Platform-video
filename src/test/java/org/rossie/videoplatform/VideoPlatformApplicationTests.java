d6572e075a91be18e6bdc9a0597efce83f8597c0
Tetteh Rose <queenefya669@gmail.com>
2024-04-29 17:04:33 -0700

    Initialized project for video platform project

---
+++ src/test/java/org/rossie/videoplatform/VideoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rossie.videoplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
